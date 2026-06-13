b81c2f818487977211829bcbcfd4dae3a87bcf5b
monodeep88 <57420958+monodeep88@users.noreply.github.com>
2026-06-13 19:58:58 +0530

    Initial generated project

---
+++ resume_q_a/__init__.py
@@ -0,0 +1,5 @@
+"""Generated Resume Q&A Assistant package."""
+
+from .app import build_assistant, describe_project, generate_response
+
+__all__ = ["build_assistant", "describe_project", "generate_response"]
